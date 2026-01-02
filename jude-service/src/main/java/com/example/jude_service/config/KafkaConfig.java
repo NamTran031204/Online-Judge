4575ff05b898bbe8f9114b5d8b871328ebee1b36
nguot <vutienannguyen1210@gmail.com>
2026-01-02 15:45:36 +0700

    kafka config(topic and docker)

--- jude-service/src/main/java/com/example/jude_service/config/KafkaConfig.java
+++ jude-service/src/main/java/com/example/jude_service/config/KafkaConfig.java
@@ -1,4 +1,15 @@
 package com.example.jude_service.config;
 
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class KafkaConfig {
+
+    @Bean
+    public NewTopic submissionProgressTopic() {
+        return new NewTopic("submission.progress", 1, (short) 1);
+    }
 }
+
