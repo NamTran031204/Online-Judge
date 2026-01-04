c178e35bfbf56ac85c9f64dc1ee4e7875a14a6ed
nguot <vutienannguyen1210@gmail.com>
2026-01-05 03:55:45 +0700

    merge code Nam - validate testcase and java

--- compile-temp/judge-309f233f-7b02-4011-8733-404f6d0deda74d446a95-868a-4756-b01a-36c477a69f78_Main.java/Main.java
+++
@@ -1,1 +0,0 @@
-import java.util.Scanner;\n public class Solution { public static void main(String[] args) { Scanner sc = new Scanner(System.in); int n = sc.nextInt(); int m = sc.nextInt(); int sum = n + m; System.out.print(sum); sc.close(); } }
